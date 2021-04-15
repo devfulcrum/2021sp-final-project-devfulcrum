d969213d6e7fa39a1759b4f697f216743b0d7986
muthu <muthu_venkataraman@yahoo.com>
2021-04-14 21:27:53 -0400

    Initial commit

---
+++ final_project/__init__.py
@@ -0,0 +1,12 @@
+# from pkg_resources import get_distribution, DistributionNotFound
+
+# code included from csci_utils pset and Professor's lecture slides
+
+try:
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="..", relative_to=__file__)
+except:  # package is not installed  # noqa: E722
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="..", relative_to=__file__)
